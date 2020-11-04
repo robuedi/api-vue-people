512c829ce8a288a1c5704e84e62632fa179ea365
Eduard Robu <robu.edi@gmail.com>
2020-11-04 14:41:23 +0000

    api person index

--- app/Http/Controllers/Api/PersonsController.php
+++ app/Http/Controllers/Api/PersonsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PersonResource;
+use App\Http\Resources\PersonResourceCollection;
 use Illuminate\Http\Request;
 use App\Models\Person;
 
@@ -16,5 +17,10 @@ class PersonsController extends Controller
     public function show(Person $person) : PersonResource
     {
         return new PersonResource($person);
+    }
+
+    public function index() : PersonResourceCollection
+    {
+        return new PersonResourceCollection(Person::paginate());
     }
 }
