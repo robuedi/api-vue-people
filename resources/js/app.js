127f02e02a5d8233efc8562c343fda1b6183f446
Eduard Robu <robu.edi@gmail.com>
2020-11-06 17:34:23 +0000

    vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,9 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import Form from './Form'
+window.Form = Form
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -19,7 +22,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('person-list-component', require('./components/PersonListComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ resources/js/components/PersonListComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="w-25">
+        <form @submit.prevent="storePerson">
+            <div class="input-group mb-3 w-100">
+                <input v-model="form.firstName" type="text" class="form-control form-control-lg" aria-label="Person first name" aria-describedby="save-person"/>
+                <input v-model="form.lastName" type="text" class="form-control form-control-lg" aria-label="Person last name" aria-describedby="save-person"/>
+                <input v-model="form.phone" type="text" class="form-control form-control-lg" aria-label="Person phone" aria-describedby="save-person"/>
+                <input v-model="form.email" type="text" class="form-control form-control-lg" aria-label="Person email" aria-describedby="save-person"/>
+                <input v-model="form.city" type="text" class="form-control form-control-lg" aria-label="Person city" aria-describedby="save-person"/>
+                <div class="input-group-append" >
+                    <button class="btn btn-success" type="submit" id="save-person"></button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                persons: '',
+                form: new Form({
+                    firstName: '',
+                    lastName: '',
+                    phone: '',
+                    email: '',
+                    city: ''
+                })
+            }
+        },
+        methods: {
+            index(){
+
+            },
+            storePerson(){
+
+            },
+            updatePerson(){
+
+            },
+            destroyPerson(){
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
