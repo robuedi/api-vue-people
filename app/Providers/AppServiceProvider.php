862fb623cfd225dbe745c1af3f4233cc48295aed
Eduard Robu <edi_cristi3@yahoo.com>
2021-03-01 13:39:28 +0000

    repository binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\PersonRepository;
+use App\Repositories\PersonRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->bind(PersonRepositoryInterface::class, PersonRepository::class);
     }
 
     /**
