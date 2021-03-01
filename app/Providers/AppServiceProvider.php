b3dc1f7b9d8966b269e4d623657a09ad4beb8461
Eduard Robu <edi_cristi3@yahoo.com>
2021-03-01 13:38:18 +0000

    remove unnecessary bindings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,21 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-
-        $this->app->bind('PersonResourceCollection', function ($app, $data) {
-            return \App\Http\Resources\v1\PersonResource::collection(reset($data));
-        });
-
-        $this->app->bind('PersonResource', function ($app, $data) {
-            //check which API version we are
-            switch (request()->route()->getPrefix()){
-                case 'api/v1/':
-                    return new \App\Http\Resources\v1\PersonResource(reset($data));
-
-                case 'api/v2/':
-                    return new \App\Http\Resources\v2\PersonResource(reset($data));
-            }
-        });
     }
 
     /**
